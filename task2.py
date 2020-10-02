2e5498adef8f0a518f4fd96126e03ff8bc5d9a5e
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-02 11:32:20 -0700

    Update task2.py

---
+++ task2.py
@@ -0,0 +1,13 @@
+import time
+names=("Lebron","Kobe","Michael","Shaq","Dennis")
+user=input("Enter a name")  
+count=1
+for i in names:
+   if i== user:
+      count=count-1
+      print("That name is on the list")   
+   if count>=5:
+      print("That name is not on the list") 
+   if i!=user:
+      count=count+1
+      continue 
