686bade0fcfacfa98e1cd6928defd1cc2e58cac4
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-02 12:30:57 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,7 +1,7 @@
 integer=input(("Enter a number")).strip()
 integer=int(integer)
 factorial=1
-if integer>=1:
-    for i in range(1,integer+1):
-        factorial= factorial*i
-print(str(integer)+"!="+str(factorial))
+for i in range(1,integer+1):
+    factorial= factorial*i
+print(str(integer)+"! is "+str(factorial))
+
